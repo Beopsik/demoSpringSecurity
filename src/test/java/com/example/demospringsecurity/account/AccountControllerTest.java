a81cf8605f063cfba352c8f6dc432af40909d650
BeobSik <tjswl756@naver.com>
2022-07-25 14:13:34 +0900

    스프링 시큐리티 테스트 1부

---
+++ src/test/java/com/example/demospringsecurity/account/AccountControllerTest.java
@@ -0,0 +1,59 @@
+package com.example.demospringsecurity.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AccountControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void index_anonymous() throws Exception{
+        mockMvc.perform(get("/").with(anonymous()))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void index_user() throws Exception{
+        mockMvc.perform(get("/").with(user("beobsik").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void index_admin() throws Exception{
+        mockMvc.perform(get("/").with(user("admin").roles("ADMIN")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void admin_user() throws Exception{
+        mockMvc.perform(get("/admin").with(user("beobsik").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void admin_admin() throws Exception{
+        mockMvc.perform(get("/admin").with(user("admin").roles("ADMIN")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+
+}
